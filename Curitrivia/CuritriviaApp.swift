5cdae12c34dd5b37e17bdcd8e48844ade495cfed
laura misolani <lmisolani>
2023-08-01 08:39:32 -0300

    Initial Commit

---
+++ Curitrivia/CuritriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CuritriviaApp.swift
+//  Curitrivia
+//
+//  Created by Laura Machado Isolani on 01/08/23.
+//
+
+import SwiftUI
+
+@main
+struct CuritriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
